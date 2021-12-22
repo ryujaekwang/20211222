1e7a13d7e0ff91b53b227aa55d414c375f3aa823
idfxgx <idfxgx@naver.com>
2021-12-22 15:38:39 +0900

    추가

---
+++ Main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int a = 1;
+	int b = 2;
+
+	cin >> a;
+	cin >> b;
+
+	int mul = a * b;
+
+	cout << "이건 계산기입니다." << endl;
+	cout << a << " *" << b << " = " << mul << " 입니다 " << endl;
+
+	return 0;
+
+}
